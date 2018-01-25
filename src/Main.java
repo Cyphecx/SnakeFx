de592e280617c39bb14974e1f712e3caec03b88e
cyphecx <whauben11@gmail.com>
2018-01-25 12:51:27 -0500

    Started work on main menu
    Have a few buttons setup up in a temporary(maybe not) Vbox layout
    
    The buttons have test print statements
    
    Method for closing the game setup and on menu button and close request for the future

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 
     Stage window;
-    Scene bScene;
+    Scene TitleScreen;
+    static int WINDOW_HEIGHT = 500;
+    static int WINDOW_WIDTH = 800;
 
     public static void main(String[] args) {
        launch(args);
@@ -17,21 +22,34 @@ public class Main extends Application{
 
     public void start(Stage primaryStage) throws Exception{
         window = primaryStage;
-        Button changeScenes = new Button();
-        changeScenes.setText("Lets change scenes");
-        changeScenes.setOnAction(e -> {
-            AlertBox.display("Hi", "Meow");
-            if(AlertBox.choice){
-                System.out.println("hel");
-            }
-            else{
-                System.out.println("na");
-            }
-        });
-        bScene = new Scene(changeScenes, 400, 400);
+        Label titleStylized = new Label("Snake now with FX");
 
-        window.setScene(bScene);
+        Button startGame = new Button();
+        startGame.setText("Start a new Game!");
+        startGame.setOnAction(e -> System.out.println("Start Game"));
+
+        Button openSettings = new Button("Settings");
+        openSettings.setOnAction(e -> System.out.println("Opened Settings"));
+
+        Button closeGame = new Button("Exit to Desktop");
+        closeGame.setOnAction(e -> quitGame());
+
+
+
+        VBox test = new VBox(10);
+        test.getChildren().addAll(titleStylized, startGame, openSettings, closeGame);
+        test.setAlignment(Pos.BASELINE_CENTER);
+
+        TitleScreen = new Scene(test, WINDOW_HEIGHT, WINDOW_WIDTH);
+        window.setScene(TitleScreen);
+        window.setOnCloseRequest(e -> {
+            e.consume();
+            quitGame();
+        });
         window.show();
     }
 
+    void quitGame(){
+        window.close();
+    }
 }
