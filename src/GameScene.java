69a51452edb032bb73d61707dd177c0068b4ff41
Wyatt Hauben <whauben11@gmail.com>
2018-02-26 10:00:12 -0500

    Adding coordinates to keep track of snake and returning to canvas
    work on threading

--- src/GameScene.java
+++ src/GameScene.java
@@ -12,11 +12,11 @@ import java.util.ArrayList;
 
 public class GameScene {
     private static Scene gameView;
-    private int CELL_INTERVEL = 20;
-    ArrayList<Rectangle> snakeSegments;
+    private double CELL_INTERVEL = 20;
+    ArrayList<Coordinate> snakeSegments;
 
     GameScene(Stage window){
-        snakeSegments = new ArrayList<Rectangle>();
+        snakeSegments = new ArrayList<Coordinate>();
         Group root = new Group();
         Canvas game = new Canvas(Main.WINDOW_WIDTH,Main.WINDOW_HEIGHT);
         GraphicsContext paint = game.getGraphicsContext2D();
@@ -25,10 +25,15 @@ public class GameScene {
         gameView = new Scene(root, window.getWidth(), window.getHeight());
         window.setScene(gameView);
 
-        snakeSegments.add(new Rectangle());
+//        snakeSegments.add(new Coordinate(4,4));
         boolean isGameRunning = true;
         while(isGameRunning){
+            System.out.println("tstetiansd");
             paintGame(paint);
+            try {
+                Thread.sleep(2000);
+            }catch(InterruptedException e){}
+            isGameRunning = false;
         }
     }
 
@@ -36,14 +41,17 @@ public class GameScene {
 
     }
 
+    private void paintGame(GraphicsContext paint){
+        paint.clearRect(0, 0, Main.WINDOW_WIDTH, Main.WINDOW_HEIGHT);
+        paint.setFill(Color.BLACK);
+//        for (Coordinate s: snakeSegments) {
+//            paint.fillRect((s.getX()*CELL_INTERVEL), (s.getY()*CELL_INTERVEL), CELL_INTERVEL, CELL_INTERVEL);
+//        }
+    }
 
     Scene getGameView(){
         return gameView;
     }
 
-    private void paintGame(GraphicsContext paint){
-        paint.setFill(Color.BLACK);
-        paint.fillRect(100,100,100,100);
 
-    }
 }
